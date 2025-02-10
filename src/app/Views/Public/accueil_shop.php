7266e13f6eadffb29a60bc88bb7513affc3cb9d9
Mélanie Bethermat <melaniebethermat@gmail.com>
2025-02-10 21:53:45 +0100

    'ajout dev'

---
+++ src/app/Views/Public/accueil_shop.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("./../includes/head_shop.php"); ?>
+<body class="body_shop_accueil" id="top_shop_accueil">
+<?php include('./../includes/header_shop.php'); ?>
+
+
+
+
+  <!-- Hero Section -->
+  <section class="hero">
+  <div class="container_banniere_accueil_shop">
+  <div class="content">
+    <h1>Seconde Main Durable et Stylée</h1>
+    <p>+700 pièces certifiées parfait état</p>
+    <div class="buttons_shop_banniere_accueil">
+      <a href="#">Femmes</a>
+      <a href="#">Enfants</a>
+      <a href="#">Accessoires</a>
+    </div>
+  </div>
+</div>
+
+  </section>
+  <?php include('./../includes/footer_shop.php');?>
+</body>
+</html>
---
+++ src/app/Views/includes/header_shop.php
@@ -0,0 +1,41 @@
+<header class="header_shop">
+  <div class="logo">
+    <img src="assets/images/logo_magasin-chic.png" alt="logo-magasin-chic-and-chill">
+  </div>
+  <nav>
+    <div class="dropdown">
+      <a href="#">Femmes</a>
+      <div class="dropdown-content">
+        <a href="#">Tout voir</a>
+        <a href="#">Vestes et Manteaux</a>
+        <a href="#">Chemises et Blouses</a>
+        <a href="#">Pulls, Sweats & Gilets</a>
+        <a href="#">Tops et T-shirts</a>
+        <a href="#">Pantalons</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <a href="#">Hommes</a>
+      <div class="dropdown-content">
+        <a href="#">Tout voir</a>
+        <a href="#">Vestes</a>
+        <a href="#">Pantalons</a>
+        <a href="#">T-shirts</a>
+      </div>
+    </div>
+    <a href="#">Marques</a>
+    <a href="#">Nouveautés</a>
+    <a href="#">Promotions</a>
+    <a href="#">Vendre</a>
+    <a href="#">Professionnels</a>
+  </nav>
+  <div class="right-section">
+    <span class="chic-and-chill">CHIC&CHILL</span>
+    <div class="icons_shop_nav_bar">
+  <a href="#"><i class="fas fa-search"></i></a> <!-- Loupe -->
+  <a href="#"><i class="fas fa-shopping-cart"></i></a> <!-- Panier -->
+  <a href="#"><i class="fas fa-user"></i></a> <!-- Profil -->
+</div>
+
+  </div>
+</header>
